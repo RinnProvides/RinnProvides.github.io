170b6b942b18329edb280825a9cb876c41a78808
RinnProvides <rinnsmarket@gmail.com>
2026-02-12 00:31:47 -0500

    Update games.ts

--- src/data/games.ts
+++ src/data/games.ts
@@ -40,7 +40,7 @@ export const games: Game[] = [
     id: 'duck-life-4',
     title: 'Duck Life 4',
     thumbnail: 'https://www.coolmathgames.com/sites/default/files/DuckLife4_OG-logo.jpg',
-    embedUrl: '  https://dnrweqffuwjtx.cloudfront.net/games/2024/wix/duck-life-4/index.html',
+    embedUrl: 'https://unblocked-games.s3.amazonaws.com/games/2024/wix/duck-life-4/index.html',
     categories: ['puzzle', 'racing', 'sports'],
     featured: true,
     isHot: true,
